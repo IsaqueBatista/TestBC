456251cf5cc20cfd40be842e2665273e35c96cd1
isaquebatista <isaquebatista1716@gmail.com>
2023-04-26 11:12:55 -0300

    Adicionando favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,11 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Isaque | Front-End Developer</title>
-        <meta name="description" content="Sou um Desenvolvedor Front-End com experiência na construção de projetos e-commerce." />
+        <title>Ong BC</title>
+        <meta name="description" content="Buscamos ajudar e melhorar a vida das pessoas através de oportunidades reais" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.jpeg" />
+
       </Head>
       <Main/>
     </div>
